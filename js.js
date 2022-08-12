73b60ce4d53acd7669d52c60617b81eda5f09c6e
Abduqodir077 <urinovabduqodir07@gmail.com>
2022-08-12 18:18:14 +0500

    add local storage

--- js.js
+++ js.js
@@ -8,3 +8,19 @@ function btn(){
 img.addEventListener("click", () => {
     document.getElementById("par").innerHTML = 0;
 })
+
+function btn(){
+    if(localStorage.btn){
+        localStorage.btn = Number(localStorage.btn)+1;
+    } else {
+        localStorage.btn = 1;
+    }
+    document.getElementById("par").innerHTML = localStorage.btn;
+}
+
+// let par2 = 0;
+
+// function btn2(){
+//     par2++;
+//     document.getElementById("par2").innerHTML = par2;
+// }
